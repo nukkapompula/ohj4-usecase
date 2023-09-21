3d77e5ca5aed23462e6315cd25b770898aaaa190
Arto Eskelinen <arto.eske@gmail.com>
2023-09-21 06:54:38 +0300

    funktion 'Kirjaudu sisään' runko

--- toiminnot.js
+++ toiminnot.js
@@ -1,1 +1,12 @@
-document.getElementById("profiilinLuominen").style.display = "none";
+function kirjauduSisaan(){
+    let nimi = document.getElementById("kayttajaNimi").value;
+    let sana = document.getElementById("salasana").value;
+    if(nimi.length == 0){
+        window.alert("Syötä ruutuun käyttäjänimesi.");
+        document.getElementById("kayttajaNimi").value = "";
+    }
+    if(sana.length == 0){
+        window.alert("Salasana on väärä!")
+        document.getElementById("salasana").value = "";
+    }
+}
